836f44dce64920102fb4479a267ad3b753213b07
Sebastian Jensen <sebastian.jensen@ga.ntig.se>
2022-11-17 14:18:15 +0100

    fixed game over screen

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+let quizForm = document.querySelector("form");
+let finishedScreen = document.querySelector(".finished");
+let pointsLabel = document.querySelector(".points-label");
 let sendButton = document.querySelector(".send-button");
+
+
 sendButton.addEventListener("click", handleForm);
+
 function handleForm() {
     let points = 0;
     let answers = document.querySelectorAll("input:checked");
     for (let answer = 0; answer < answers.length; answer++) {
         points += parseInt(answers[answer].value, 10);
     }
-    alert(`You got ${points} / 3 right`);
+    quizForm.classList.toggle("hidden");
+    pointsLabel.innerHTML = points;
+    finishedScreen.classList.toggle("hidden");
 }
